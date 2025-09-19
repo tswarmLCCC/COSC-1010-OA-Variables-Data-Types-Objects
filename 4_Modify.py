743c6649a260513308c87b45c1d3bc9f07831e12
Trevor Swarm <tswarm@lccc.wy.edu>
2025-09-19 16:41:34 -0600

    Update to-do list management and print statements
    Added functionality to manage a to-do list by adding a new task and removing a completed one.

--- 4_Modify.py
+++ 4_Modify.py
@@ -18,3 +18,30 @@ occupation = "Programmer"
 print("User:")
 print(username)
 print("is an Programmer")
+
+
+"""
+Modify Activity 2: Managing a To-Do List
+
+Task: You are given a list representing a simple to-do list.
+
+1.  A new task has come up: "Pay bills". Add this task to the END of the `todos` list.
+2.  You have already completed the "Call mom" task. Remove this specific task from the list.
+3.  Modify the final print statement to use an f-string that prints the corrected list.
+
+The final output should be:
+Your final to-do list: ['Buy groceries', 'Clean the house', 'Pay bills']
+"""
+
+# Starter Code
+todos = ["Buy groceries", "Call mom", "Clean the house"]
+print(f"Starting to-do list: {todos}")
+
+# TODO: Add "Pay bills" to the end of the list
+
+
+# TODO: Remove "Call mom" from the list
+
+
+# TODO: Modify this print statement to use an f-string
+print("Your final to-do list:", todos)
